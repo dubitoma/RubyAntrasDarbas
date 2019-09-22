b9e035847fd617ed448c9cff6e5a0e3c4c501dc6
arturtunevic <50708444+arturtunevic@users.noreply.github.com>
2019-09-22 13:12:17 +0300

    Add files via upload

---
+++ Rectangle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace RectanglePrgr1uzd
+{
+    class Rectangle
+    {
+        public double plotis;
+        public double ilgis;
+        public float x, y;
+
+       public Rectangle(double plot, double ilg, float xF, float yF)
+        {
+            plotis = plot;
+            ilgis = ilg;
+            x = xF;
+            y = yF;
+        }
+
+        public double getPlotas()
+        {
+            return ilgis * plotis;
+        }
+
+
+        public double getPerimetras()
+        {
+            double prm;
+            prm = ilgis + plotis;
+            return prm*2;
+        }
+
+        public double getIstrizaine()
+        {
+            return Math.Sqrt((Math.Pow(ilgis, 2) + Math.Pow(plotis, 2)));
+        }
+
+        public float getCentreX()
+        {
+            float d = (float)ilgis;
+            return x + d / 2;
+        }
+
+        public float getCentreY()
+        {
+            float d = (float)plotis;
+            return y + d/ 2;
+        }
+
+    }
+}
