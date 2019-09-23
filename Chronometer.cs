15691ee5d400e6c9e6dab8f7b5b59767d059c666
dubitoma <38418546+dubitoma@users.noreply.github.com>
2019-09-23 13:31:38 +0300

    Pridedu source koda

---
+++ Chronometer.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+
+namespace AntrasDarbas
+{
+    class Chronometer
+    {
+        // atributai
+        Stopwatch stopwatch = new Stopwatch();
+        Stopwatch gettime = new Stopwatch();
+        Stopwatch pauze = new Stopwatch();
+        bool isrunning;
+        bool pauzeslaikas;
+        TimeSpan laikas;
+
+        // konstruktoriai
+        public Chronometer()
+        {
+            isrunning = false;
+            pauzeslaikas = false;
+          
+        }
+
+
+        // metodai
+        public void Startas()
+        {
+       
+            stopwatch.Start();
+
+            Console.WriteLine("Laikas skaiciuoti pradetas!");
+            Console.WriteLine("laikas: {0:hh\\:mm\\:ss}", stopwatch.Elapsed);
+
+           
+
+        }
+      public TimeSpan Stopas()
+        {
+ 
+            stopwatch.Stop();
+            gettime.Stop();        
+            laikas = stopwatch.Elapsed;
+            stopwatch.Reset(); //atstatomas laikas
+            gettime.Reset();  // <------ SAME
+            isrunning = false;
+            return laikas;
+
+           
+        }
+
+        public TimeSpan Getlaptime()
+        {
+            if (isrunning) // tikrinama ar jau pradedas getlap laikmatis
+            {
+                gettime.Stop();
+                laikas = gettime.Elapsed;
+                gettime.Reset();
+                gettime.Start();
+                return laikas;
+            }
+            else
+            {
+                
+                isrunning = true; // nustatoma true reiksme tikrinimui
+                gettime.Start();
+                return stopwatch.Elapsed;
+
+            }
+            
+  
+        }
+
+        public TimeSpan Pauze()
+        {
+            if(pauzeslaikas)
+            {
+                
+                stopwatch.Start();
+                gettime.Start();
+                pauze.Stop();
+               
+                laikas = pauze.Elapsed;
+                pauze.Reset();
+                pauzeslaikas = false;
+                return laikas;
+
+            }
+            else
+            {
+           
+            stopwatch.Stop();
+            gettime.Stop();
+            pauze.Start();
+            pauzeslaikas = true;
+            return pauze.Elapsed;
+            }
+     
+
+        }
+
+        public void pat()
+        {
+            Console.WriteLine("esamas laikas: {0:hh\\:mm\\:ss}", stopwatch.Elapsed);
+        }
+
+
+    }
+}
