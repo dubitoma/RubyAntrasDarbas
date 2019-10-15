4c600aa95b8b2681eefa3ac4ca168bb89fea851f
dubitoma <38418546+dubitoma@users.noreply.github.com>
2019-10-15 22:58:42 +0300

    Robocop fix chronometras
    Robocop fix chronometras

---
+++ Chronometras_fixed.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+# :nodoc:
+class Stopwatch
+  def start
+    @start = Time.now
+    puts 'Pradzia: ' + @start.to_s
+  end
+
+  def elapsed_time
+    now = Time.now
+    elapsed = now - @start
+    puts 'Pradzia: ' + @start.to_s
+    puts 'Dabartinis: ' + now.to_s
+    puts 'Praejas laikas: ' + elapsed.to_s + ' seconds'
+    @start = 0
+  end
+
+  def show_time
+    now = Time.now
+    elapsed = now - @start
+    puts ''
+    puts 'Praejo: ' + elapsed.to_s + ' seconds'
+  end
+end
+
+i = 0
+
+while i != 1
+
+  puts '1. Pradeti'
+  puts '2. Stabdyti'
+  puts '3. Rodyti esama'
+  puts '4. exit'
+
+  s = Stopwatch.new
+
+  x = gets.chomp.to_i
+
+  case x
+  when 1
+    s.start
+  when 2
+    s.elapsed_time
+  when 3
+    s.show_time
+  when 4
+    i = 1
+    puts 'bye'
+  else
+    "You gave me #{x} -- I have no idea what to do with that."
+  end
+
+end
